7efe519c45277067e89527fed0fa2eea5ace607c
hakilebara <frederic.soumare@gmail.com>
2026-01-13 18:27:54 +0100

    Create Reader class
    The first draft of a binary parsing class

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,9 +1,9 @@
-export function readULEB128(view:DataView<ArrayBuffer>, startOffset:number): {value:number, offset:number} {
+export function readULEB128(bytes:Uint8Array<ArrayBuffer|ArrayBufferLike>, startOffset:number): {value:number, offset:number} {
   let value = 0;
   let shift = 0;
   let offset = startOffset;
   while (true) {
-    const byte = view.getUint8(offset);
+    const byte = bytes[offset];
     const data = byte & 0x7F;
     offset++;
     value |= data << shift;
@@ -16,4 +16,38 @@ export function readULEB128(view:DataView<ArrayBuffer>, startOffset:number): {va
   }
 
   return { value, offset }
+}
+
+export class Reader {
+  #data:Uint8Array;
+  offset:number = 0;
+
+  constructor(buffer:Uint8Array) {
+    this.#data = new Uint8Array(buffer);
+  }
+
+  toss(n:number):void {
+    if (this.offset + n > this.#data.byteLength) throw new Error("max buffer size exceeded"); 
+    this.offset += n;
+  }
+
+  takeU8():number {
+    const byte = this.#data[this.offset];
+    this.offset++;
+    return byte;
+  }
+
+  takeULEB128():number {
+    let result = 0;
+    let shift = 0;
+    while (true) {
+      const byte = this.takeU8();
+      result |= (byte & 0x7F) << shift;
+
+      if ((byte & 0x80) === 0) break;
+
+      shift += 7;
+    }
+    return result;
+  }
 }
